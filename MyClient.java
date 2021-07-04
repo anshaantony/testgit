ee6bb8f4db42dfcff2bed7478cf94209c5b2d4fc
anshaantony <anshaantonymanavalan@gmail.com>
2021-07-04 17:20:47 +0530

    committed

--- MyClient.java
+++ MyClient.java
@@ -1,6 +1,6 @@
 import java.net.*;
 import java.io.*;
-class MyClient2{
+class MyClient{
 public static void main(String args[])throws Exception{
 Socket s=new Socket("localhost",3333);
 DataInputStream din=new DataInputStream(s.getInputStream());
