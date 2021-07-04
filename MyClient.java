509627e5abe8ffa84c782afd786cc4de0b8ac42b
anshaantony <anshaantonymanavalan@gmail.com>
2021-07-04 17:39:26 +0530

    committed

--- MyClient.java
+++ MyClient.java
@@ -1,19 +1,26 @@
-import java.net.*;
-import java.io.*;
-class MyClient{
-public static void main(String args[])throws Exception{
-Socket s=new Socket("localhost",3333);
-DataInputStream din=new DataInputStream(s.getInputStream());
-DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-String str="",str2="";
-while(!str.equals("stop")){
-str=br.readLine();
-dout.writeUTF(str);
+import java.net.*; 
+import java.io.*; 
+class MyClient
+{
+
+public static void main(String args[])throws IOException{ Socket s = new Socket("localhost",3333);
+DataInputStream din = new DataInputStream(s.getInputStream()); 
+DataOutputStream dout = new DataOutputStream(s.getOutputStream()); 
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+String str= " "; String str2=" ";
+while(!str.equals("stop"))
+
+{
+
+str=br.readLine(); System.out.println("Echo :"+str); dout.writeUTF(str);
 dout.flush();
-System.out.println("Server says: "+str2);
+str2=din.readUTF(); System.out.println("server says :"+str2);
 }
+
 dout.close();
+
 s.close();
 
-}}
+}
+
+}
--- MyServerEcho.java
+++ MyServerEcho.java
@@ -1,20 +1,23 @@
-import java.net.*;
-import java.io.*;
+import java.net.*; 
+import java.io.*; 
 class MyServerEcho{
-public static void main(String args[])throws Exception{
-ServerSocket ss=new ServerSocket(3333);
-Socket s=ss.accept();
-DataInputStream din=new DataInputStream(s.getInputStream());
+public static void main(String args[])throws IOException{ ServerSocket ss = new ServerSocket(3333);
+Socket s = ss.accept();
+DataInputStream din = new DataInputStream(s.getInputStream()); 
 DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-String str="",str2="";
-while(!str.equals("stop")){
-str=din.readUTF();
-System.out.println("client says: "+str);
-dout.writeUTF(str);
+BufferedReader br = new BufferedReader(new BufferedReader(new InputStreamReader(System.in))); 
+String str = " ",str2=" ";
+while(!str.equals("stop")){ str = din.readUTF();
+System.out.println("client says:"+str); str2=br.readLine(); dout.writeUTF(str2);
 dout.flush();
+
 }
+
 din.close();
+
 s.close();
+
 ss.close();
-}}
+}
+
+}
